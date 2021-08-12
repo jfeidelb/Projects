3ab41efb246045fa30525fc4c9906f6b491e3180
John F <zandersf@me.com>
2021-08-12 13:50:38 -0400

    Add README.me and comments

--- PythonTextGame.py
+++ PythonTextGame.py
@@ -3,16 +3,17 @@ import operator
 
 print("This is an educational game where you will answer simple math questions.")
 print("How many questions would you like to answer?")
-
 questionInput = input()
 
 def MathQuestion(questionInput):
     questionLoop = 0
     score = 0
     try:
+        #loop to control number of questions
         while questionLoop < int(questionInput):
             value1 = randint(1, 20)
             value2 = randint(1, 20)
+            #variable to assign mathematical operation
             operationVariable = randint(0, 3)
             if operationVariable == 0:
                 correctMathAnswer = operator.add(value1, value2)
@@ -30,9 +31,11 @@ def MathQuestion(questionInput):
                 correctMathAnswer = operator.truediv(value1, value2)
                 operation = "/"
             print()
+            #asking the math question to the user
             print("What is " + str(value1) + operation + str(value2) + "?")
             userInput = input()
             try:
+                #checks the users answer with the correct answer
                 if float(userInput) == correctMathAnswer:
                     print("Yes! That is the correct answer!")
                     score += 1
@@ -40,9 +43,12 @@ def MathQuestion(questionInput):
                 else:
                     print("That is not the correct answer.")
                     print("Your score is: " + str(score))
+            #except statement to ensure a number is answered
             except ValueError:
                 print("Please enter a numerical answer.")
             questionLoop += 1
+
+    #except statement to ensure a number is entered
     except ValueError:
         print("Please enter a number!")
         questionInput = input()
--- Upload.bat
+++
@@ -1,3 +0,0 @@
-cd A:\Projects
-"C:\Program Files\Git\git-bash.exe" '.\Upload.sh'
-exit
